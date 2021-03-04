b2515901bbe604bec684635ea4204578a21505dc
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-03-04 09:55:00 -0500

    added a few changes, will bug test soon

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ const Balls = require('discord.js')
 const fs = require('fs')
 const client = new Balls.Client()
 
+const token = 'a1b2c3d4e5f6g8h9'
+const prefix = '!'
+
 client.on('ready', ready => {
     console.log("Smell Bot is now online!")
     console.log("Please note, I did this in a rush so I could go play siege, I was challenged and that is why I didn't take the time to clean the code, or make it 80x more effecient and clean. This is a BAD representation of my work.")
@@ -10,7 +13,7 @@ client.on('ready', ready => {
 
 client.on('message', async message => {
     const args = message.content.trim().split(/ +/g);
-    if(message.content.startsWith("!smell")) {
+    if(message.content.startsWith(`${prefix}smell`)) {
         if(args[1]){
             const embed = new Balls.MessageEmbed()
             .setTitle("Please Read Below!")
@@ -32,11 +35,13 @@ client.on('message', async message => {
         console.log(`${message.author.username} ran the !smell command`)
         message.delete()
         }
-    } else if(message.content.startsWith("!credits")) {
+    } else if(message.content.startsWith(`${prefix}credits`)) {
         message.channel.send(`My creator is Hyperz#0001`).then(msg => msg.delete({ timeout: 9000 }))
         message.delete()
         console.log(`${message.author.username} ran the !credits command and I responded with the creator of the bot (Hyperz#0001)`)
+    } else if(message.mentions.users.first().username = client.bot.username) {
+        message.channel.send(`My prefix is ${prefix}`).then(msg => msg.delete({ timeout: 9000 }))
     }
 });
 
-client.login('your-bot-token')
+client.login(token)
